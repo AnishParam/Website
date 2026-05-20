6bdb79f0eaaef2c3d086a730213095b90cc810bc
AnishParam <anishtheking11@gmail.com>
2026-05-20 17:14:03 -0400

    Revert "changes testing"
    This reverts commit c13f822c99acf7e5be68dce65b5afcc8f683c0d2.

--- scriptA.js
+++ scriptA.js
@@ -3,28 +3,16 @@ clicksound.preload = 'auto';
 clicksound.play();
 clicksound.pause();
 
-function isIOS() {
-    return /iPad|iPhone|iPod/.test(navigator.userAgent) ||
-           (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-}
-
 function openPDF(filePath) {
     const pdfOverlay = document.getElementById("pdfOverlay");
+
     const oldViewer = document.getElementById("pdfViewer");
+    const newViewer = document.createElement("object");
+    newViewer.id = "pdfViewer";
+    newViewer.type = "application/pdf";
+    newViewer.data = filePath;
 
-    if (isIOS()) {
-        const iframe = document.createElement("iframe");
-        iframe.id = "pdfViewer";
-        iframe.src = filePath;
-        oldViewer.replaceWith(iframe);
-    } else {
-        const newViewer = document.createElement("object");
-        newViewer.id = "pdfViewer";
-        newViewer.type = "application/pdf";
-        newViewer.data = filePath;
-        oldViewer.replaceWith(newViewer);
-    }
-
+    oldViewer.replaceWith(newViewer);
     pdfOverlay.style.display = "block";
 }
 
@@ -109,8 +97,6 @@ window.onload = () => {
     
     pdfOverlay.addEventListener("click", () => {
         pdfOverlay.style.display = "none";
-        const viewer = document.getElementById("pdfViewer");
-        viewer.data = "";
-        viewer.src = "";
+        document.getElementById("pdfViewer").src = "";
     });
 };
